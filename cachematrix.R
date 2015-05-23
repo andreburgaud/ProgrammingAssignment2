97084e1e1afdc1d88fd02701434bc5b144dc03fe
andre <andre.burgaud@gmail.com>
2015-05-23 01:05:31 -0500

    Implemented cachematrix and basic tests.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+# =============================================================================
+# Implementation and usage of a cacheable matrix. Cachable matrix
+# caches its inverse upon first computation.
+# Example:
+# m <- makeCacheMatrix(matrix(rnorm(4.9e+05), nrow=700, ncol=700))
+# i <- cacheSolve(m)
+# =============================================================================
 
-## Write a short comment describing this function
 
+# Constructs a cachable matrix.
+# Arg:
+#   x: Matrix object, default to empty matrix if no argument.
+# Return:
+#   Cacheable matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+    s <- NULL
+    set <- function(y) {
+        x <<- y
+        s <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solve) s <<- solve
+    getinverse <- function() s
+    list(set = set, get = get,
+        setinverse = setinverse,
+        getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+# Computes inverse of a matrix.
+# Args:
+#     x: Cacheable matrix (built with makeCacheMatrix)
+#     ...: Extra arguments passed to 'solve' function
+# Return:
+#     Matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    s <- x$getinverse()
+    if (!is.null(s)) {
+        message("Getting cached inverse")
+        return(s)
+    }
+    data <- x$get()
+    s <- solve(data, ...)
+    x$setinverse(s)
 }
---
+++ test_cachematrix.R
@@ -0,0 +1,43 @@
+# Basic cachematrix tests
+# To execute: Rscript test_cachematrix.R
+
+source("cachematrix.R")
+
+# =============================================================================
+message("Data preparation")
+l = list()
+n = 5
+for(i in 1:n) {
+    l[[i]] <- makeCacheMatrix(matrix(rnorm(4.9e+05), nrow=700, ncol=700))
+}
+
+# =============================================================================
+message("Initial matrix inverse computation")
+for(i in 1:n) {
+    print(system.time(cacheSolve(l[[i]])))
+}
+
+# =============================================================================
+message("Validate performance with caching")
+for(i in 1:n) {
+    print(system.time(cacheSolve(l[[i]])))
+}
+
+# =============================================================================
+message("Test usage of set")
+m <- matrix(rnorm(4.9e+05), nrow=700, ncol=700)
+cm <- makeCacheMatrix(m)
+i <- cacheSolve(cm) # Computes inverse
+i <- cacheSolve(cm) # Gets inverse from cache
+
+# Reset internal matrix of cacheable matrix (exercise 'set' function)
+cm$set(matrix(rnorm(4.9e+05), nrow=700, ncol=700))
+i <- cacheSolve(cm) # Computes inverse
+i <- cacheSolve(cm) # Gets inverse from cache
+
+# =============================================================================
+message("Validate correctness")
+# Fails if caching of regular matrix different than cached matrix)
+m <- matrix(rnorm(4.9e+05), nrow=700, ncol=700)
+cm <- makeCacheMatrix(m)
+stopifnot(all(head(solve(m)) == head(cacheSolve(cm))))
